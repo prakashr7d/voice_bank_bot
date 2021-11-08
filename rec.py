4205d656cfefa11f82a5efb54c65ed65ada1ef4b
prakashr7d <prakashr7d@gmail.com>
2021-11-08 16:11:26 +0530

    implemented

---
+++ rec.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "SERVICE_ACCOUNT_KEY.JSON"
+
+r = sr.Recognizer()
+file = sr.Microphone(device_index=0)
+
+with file as source:
+    print("say something!!.....")
+    audio = r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+
+try:
+    recog = r.recognize_google_cloud(audio, language='en-US')
+    print("You said: " + recog)
+except sr.UnknownValueError as u:
+    print(u)
+    print("Google Cloud Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Cloud Speech Recognition service; {0}".format(e))  
