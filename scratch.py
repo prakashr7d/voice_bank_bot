3304958b4aef00b4f7a4be3ec42399cd4aaff169
prakashr7d <prakashr7d@gmail.com>
2021-12-06 21:09:59 +0530

    bug on error

--- scratch.py
+++ scratch.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "ACd25b0b811ff8f87820ca9fbe03f75ded"
-auth_token = "9b3f1cc7aad65389f3b286b6aceb2c3e"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 call = client.calls \
